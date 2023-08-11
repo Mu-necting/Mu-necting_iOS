103123e92f11148de9a9be85eb4b373a34901116
LeeHyeonHo-127 <swethom1@naver.com>
2023-08-11 14:51:21 +0900

    [Feat] APIServices 디렉토리 생성

---
+++ Munecting_iOS/Munecting_iOS/Sources/APIServices/TrackService.swift
@@ -0,0 +1,6 @@
+
+import Foundation
+
+struct TrackServe{
+    static let share = TrackServe()
+}
--- Munecting_iOS/Munecting_iOS/Sources/ViewControllers/HomeViewController.swift
+++ Munecting_iOS/Munecting_iOS/Sources/ViewControllers/HomeViewController.swift
@@ -36,11 +36,20 @@ class HomeViewController: UIViewController {
         
         
         if let music = musics[0]{
+        
             
             //첫 번째 노래 재생
             let musicURL = URL(string: music.musicPre)
             self.audioPlayer = AVPlayer(url: musicURL!)
             self.audioPlayer?.play()
+
+            
+            
+//            DispatchQueue.main.asyncAfter(deadline: .now() + 35) {
+//                self.audioPlayer = AVPlayer(url: musicURL!)
+//                self.audioPlayer?.play()
+//            }
+//
             
             //첫 번재 노래 커버 이미지 설정
             let albumImage = getImage(url: URL(string: music.coverImage)!)
@@ -106,6 +115,11 @@ class HomeViewController: UIViewController {
             self.audioPlayer = AVPlayer(url: musicURL!)
             self.audioPlayer?.play()
             
+//            DispatchQueue.main.asyncAfter(deadline: .now() + 35) {
+//                self.audioPlayer = AVPlayer(url: musicURL!)
+//                self.audioPlayer?.play()
+//            }
+            
             //앨범 커버 이미지 설정
             let albumImage = getImage(url: URL(string: music.coverImage)!)
             backgroundImageView.image = albumImage.applyBlur(radius: 5.0)
@@ -142,6 +156,10 @@ class HomeViewController: UIViewController {
             let musicURL = URL(string: music.musicPre)
             self.audioPlayer = AVPlayer(url: musicURL!)
             self.audioPlayer?.play()
+//            DispatchQueue.main.asyncAfter(deadline: .now() + 35) {
+//                self.audioPlayer = AVPlayer(url: musicURL!)
+//                self.audioPlayer?.play()
+//            }
             
             //앨범 커버 이미지 설정
             let albumImage = getImage(url: URL(string: music.coverImage)!)
@@ -199,15 +217,29 @@ class HomeViewController: UIViewController {
     
     //music 갖고오기
     func getMusic(){
-        let music1:Music = Music(name: "New Jeans", coverImage: "https://is4-ssl.mzstatic.com/image/thumb/Music112/v4/4e/64/34/4e64344b-3ac6-c503-2c41-257a15401416/192641873096_Cover.jpg/100x100bb.jpg", genre: "K-POP", musicPre: "https://audio-ssl.itunes.apple.com/itunes-assets/AudioPreview122/v4/13/79/51/13795136-c3d0-1191-cc43-dcc16579a2f2/mzaf_8604667655746746744.plus.aac.p.m4a", musicPull: "https://www.youtube.com/watch?v=o8RkbHv2_a0", replyCnt: 1, archiveId: 1)
+        let music1:Music = Music(name: "NewJeans", coverImage: "https://is4-ssl.mzstatic.com/image/thumb/Music112/v4/4e/64/34/4e64344b-3ac6-c503-2c41-257a15401416/192641873096_Cover.jpg/100x100bb.jpg", genre: "K-POP", musicPre: "https://audio-ssl.itunes.apple.com/itunes-assets/AudioPreview122/v4/13/79/51/13795136-c3d0-1191-cc43-dcc16579a2f2/mzaf_8604667655746746744.plus.aac.p.m4a", musicPull: "https://www.youtube.com/watch?v=o8RkbHv2_a0", replyCnt: 1, archiveId: 1)
         let music2:Music = Music(name: "Doja Cat", coverImage: "https://is2-ssl.mzstatic.com/image/thumb/Music116/v4/26/fe/b2/26feb2d2-c11b-cf95-4b93-d57321103566/196871142090.jpg/100x100bb.jpg", genre: "Hip Hop", musicPre: "https://audio-ssl.itunes.apple.com/itunes-assets/AudioPreview116/v4/fe/2a/9a/fe2a9a4e-8afd-a670-7b25-e7d03d1f5522/mzaf_10350104113929572724.plus.aac.p.m4a", musicPull: "https://www.youtube.com/watch?v=XweT8chMepU", replyCnt: 1, archiveId: 1)
         let music3:Music = Music(name: "Charlie Puth", coverImage: "https://is3-ssl.mzstatic.com/image/thumb/Music125/v4/a8/e2/1b/a8e21b3b-9c8d-2974-2318-6bcd4c9d2370/075679884336.jpg/100x100bb.jpg", genre: "Pop", musicPre: "https://audio-ssl.itunes.apple.com/itunes-assets/AudioPreview126/v4/b5/32/d4/b532d45b-0e4d-6bf3-d7b5-e02007877318/mzaf_10109990520611630125.plus.aac.p.m4a", musicPull: "https://www.youtube.com/watch?v=6QYcd7RggNU", replyCnt: 1, archiveId: 1)
         let music4:Music = Music(name: "Sebadoh", coverImage: "https://is2-ssl.mzstatic.com/image/thumb/Music/d5/80/bd/mzi.nybhumzg.tif/100x100bb.jpg", genre: "Pop", musicPre: "https://audio-ssl.itunes.apple.com/itunes-assets/AudioPreview71/v4/bc/ae/b4/bcaeb4a0-0127-65f2-4eb2-75e82b08a95c/mzaf_4222798042305173586.plus.aac.p.m4a", musicPull: "https://www.youtube.com/watch?v=r67UF7Wpl08", replyCnt: 1, archiveId: 1)
+        let music5:Music = Music(name: "Beenzino", coverImage: "https://is1-ssl.mzstatic.com/image/thumb/Music116/v4/76/bc/37/76bc37d2-283d-f252-716e-183c268b8a87/197189280801.jpg/100x100bb.jpg", genre: "Hip-Hop", musicPre: "https://audio-ssl.itunes.apple.com/itunes-assets/AudioPreview116/v4/fc/05/e9/fc05e999-0e4f-2a28-e21d-3fd55a671208/mzaf_13793805966112651018.plus.aac.p.m4a", musicPull: "https://www.youtube.com/watch?v=r67UF7Wpl08", replyCnt: 1, archiveId: 1)
+        
+        let music6:Music = Music(name: "250", coverImage: "https://is4-ssl.mzstatic.com/image/thumb/Music126/v4/b5/9c/27/b59c2792-bb97-2a40-f482-c6d5b3ae1cea/196626603180.jpg/100x100bb.jpg", genre: "Hip-Hop", musicPre: "https://audio-ssl.itunes.apple.com/itunes-assets/AudioPreview126/v4/79/97/b3/7997b3fd-b8b8-0540-787a-0ef0988e807c/mzaf_6188522879362145428.plus.aac.p.m4a", musicPull: "https://www.youtube.com/watch?v=r67UF7Wpl08", replyCnt: 1, archiveId: 1)
+        let music7:Music = Music(name: "C JAMM", coverImage: "https://is3-ssl.mzstatic.com/image/thumb/Music115/v4/02/6a/99/026a998e-835f-81a9-d5e5-f0fbf091caf7/cover-C_JAMM_NEW.jpg/100x100bb.jpg", genre: "Hip-Hop", musicPre: "https://audio-ssl.itunes.apple.com/itunes-assets/AudioPreview125/v4/2c/7d/fc/2c7dfcb8-4c6b-f691-9c76-8fe499c3d6b7/mzaf_3230752141782334299.plus.aac.p.m4a", musicPull: "https://www.youtube.com/watch?v=r67UF7Wpl08", replyCnt: 1, archiveId: 1)
+        let music8:Music = Music(name: "Travis Scott", coverImage: "https://is2-ssl.mzstatic.com/image/thumb/Music126/v4/09/7d/b0/097db06f-8403-3cf7-7510-139e570ca66b/196871341882.jpg/100x100bb.jpg", genre: "Hip-Hop", musicPre: "https://audio-ssl.itunes.apple.com/itunes-assets/AudioPreview116/v4/f6/c0/d8/f6c0d812-9f77-ec3b-4e93-97d9264f1949/mzaf_8903675728836112418.plus.aac.p.m4a", musicPull: "https://www.youtube.com/watch?v=r67UF7Wpl08", replyCnt: 1, archiveId: 1)
+        let music9:Music = Music(name: "Kanye West", coverImage: "https://is2-ssl.mzstatic.com/image/thumb/Music115/v4/3c/56/e7/3c56e717-06a0-b67d-e694-9b6e6e43a5a8/13UAAIM08444.rgb.jpg/100x100bb.jpg", genre: "Hip-Hop", musicPre: "https://audio-ssl.itunes.apple.com/itunes-assets/AudioPreview112/v4/0c/10/8f/0c108f4d-c3dc-4374-ceb3-080aaaadc341/mzaf_17782097658048434538.plus.aac.p.m4a", musicPull: "https://www.youtube.com/watch?v=r67UF7Wpl08", replyCnt: 1, archiveId: 1)
+        let music10:Music = Music(name: "NewJeans", coverImage: "https://is4-ssl.mzstatic.com/image/thumb/Music126/v4/63/e5/e2/63e5e2e4-829b-924d-a1dc-8058a1d69bd4/196922462702_Cover.jpg/100x100bb.jpg", genre: "Hip-Hop", musicPre: "https://audio-ssl.itunes.apple.com/itunes-assets/AudioPreview126/v4/98/66/db/9866dbb4-6e6f-eedd-ea1c-4832542b8f3e/mzaf_1940197871229851903.plus.aac.p.m4a", musicPull: "https://www.youtube.com/watch?v=r67UF7Wpl08", replyCnt: 1, archiveId: 1)
+        
         
         self.musics.append(music1)
         self.musics.append(music2)
         self.musics.append(music3)
         self.musics.append(music4)
+        self.musics.append(music5)
+        self.musics.append(music6)
+        self.musics.append(music7)
+        self.musics.append(music8)
+        self.musics.append(music9)
+        self.musics.append(music10)
         
     }
 }
