cb5234e4a6f4c36118d22816c4a0872ec43e8572
seonwoonam <potatomoney716@gmail.com>
2023-08-21 19:06:00 +0900

    [FEAT] : gitignore 추가 및 비밀키 설정

--- Munecting_iOS/Munecting_iOS/Sources/AppDelegate.swift
+++ Munecting_iOS/Munecting_iOS/Sources/AppDelegate.swift
@@ -18,7 +18,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        KakaoSDK.initSDK(appKey: "755c8f13785ee882d8b39d1f05019ac1")
+        KakaoSDK.initSDK(appKey: Bundle.main.infoDictionary?["KAKAOAPPKEY"] as! String)
     
         return true
     }
